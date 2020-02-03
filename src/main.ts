0a48e12f66e346a119adb67132c0b0fbd252dcc2
Toby Mahnert <toby.mahnert@rocketboots.com>
2020-02-03 22:16:55 +1100

    lols

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,13 @@
-import './polyfills';
+import 'hammerjs';
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DemoMaterialModule} from './app/material-module';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
-import {SidenavResponsiveExample} from './app/sidenav-responsive-example';
-
-@NgModule({
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    DemoMaterialModule,
-    MatNativeDateModule,
-    ReactiveFormsModule,
-  ],
-  entryComponents: [SidenavResponsiveExample],
-  declarations: [SidenavResponsiveExample],
-  bootstrap: [SidenavResponsiveExample],
-  providers: []
-})
-export class AppModule {}
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-
-
-/**  Copyright 2019 Google LLC. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
+  .catch(err => console.log(err));
