c1e0546276b4540363b1983401a565695686f23d
Toby Manhert <tobymanhert@Admins-MacBook-Pro.local>
2023-02-18 17:17:06 +1100

    Added new configrations as I started working out again

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-
+  firebase: {
+    apiKey: "AIzaSyBFvQvulkdv_n5V5MSjs1bz_yMOltf3K9c",
+    authDomain: "tobyfit-e8dca.firebaseapp.com",
+    projectId: "tobyfit-e8dca",
+    storageBucket: "tobyfit-e8dca.appspot.com",
+    messagingSenderId: "2857896900",
+    appId: "1:2857896900:web:6fe7194ea1f06728097291"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,12 @@
 
 export const environment = {
   production: false,
-
+  firebase: {
+    apiKey: "AIzaSyBFvQvulkdv_n5V5MSjs1bz_yMOltf3K9c",
+    authDomain: "tobyfit-e8dca.firebaseapp.com",
+    projectId: "tobyfit-e8dca",
+    storageBucket: "tobyfit-e8dca.appspot.com",
+    messagingSenderId: "2857896900",
+    appId: "1:2857896900:web:6fe7194ea1f06728097291"
+  }
 };
